1245aaa4b34b4b0c7a9f96b908ec401e119aae24
Seph Gentle <me@josephg.com>
2023-11-16 11:36:34 +1100

    Made build-web script compile both normal and minified code

--- build-web.js
+++ build-web.js
@@ -2,23 +2,33 @@ import { build } from 'esbuild'
 import { solidPlugin } from 'esbuild-plugin-solid'
 import fs from 'fs'
 
-;(async () => {
-  const result = await build({
-    entryPoints: ['web/index.tsx'],
-    bundle: true,
-    minify: true,
-    logLevel: 'info',
-    sourcemap: true,
-    outfile: 'public/bundle.js',
-    metafile: true,
-    tsconfig: 'web/tsconfig.json',
-    plugins: [solidPlugin({
-      solid: {
-        generate: 'dom',
-        hydratable: false,
-      }
-    })],
-  })
+// of type BuildOptions.
+const options = {
+  entryPoints: ['web/index.tsx'],
+  bundle: true,
+  // minify: true,
+  logLevel: 'info',
+  sourcemap: true,
+  outfile: 'public/bundle.js',
+  metafile: true,
+  tsconfig: 'web/tsconfig.json',
+  plugins: [solidPlugin({
+    solid: {
+      generate: 'dom',
+      hydratable: false,
+    }
+  })],
+}
 
+;(async () => {
+  const result = await build(options)
   fs.writeFileSync('esbuild-meta.json', JSON.stringify(result.metafile))
+
+  // And also minify, to see the file size difference.
+  const minResult = await build({
+    ...options,
+    minify: true,
+    outfile: 'public/bundle.min.js',
+  })
+  // fs.writeFileSync('esbuild-meta.json', JSON.stringify(result.metafile))
 })()
